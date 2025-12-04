1ec2d5738cce9c892e47740eed9f5d2a08cf84f0
Dall'Agnol <arthur.set08@gmail.com>
2025-12-04 17:23:37 -0300

    04/12/25

--- java/script.js
+++ java/script.js
@@ -1,12 +1,44 @@
-const btn = document.getElementById("temaBtn");
-const body = document.body;
+const form = document.getElementById("form");
+const lista = document.getElementById("lista");
 
-btn.addEventListener("click", () => {
-    body.classList.toggle("dark");
+let alunos = [];  // Nosso array de objetos
 
-    if (body.classList.contains("dark")) {
-        btn.textContent = "Modo Light";
-    } else {
-        btn.textContent = "Modo Dark";
-    }
+// Função pra renderizar a lista
+function renderLista() {
+  lista.innerHTML = "";
+
+  alunos.forEach((aluno, index) => {
+    const li = document.createElement("li");
+    li.innerHTML = `${aluno.nome} — ${aluno.idade} anos`;
+
+    const btn = document.createElement("button");
+    btn.textContent = "Excluir";
+    btn.classList.add("remover");
+    btn.onclick = () => removerAluno(index);
+
+    li.appendChild(btn);
+    lista.appendChild(li);
+  });
+}
+
+// Função pra remover
+function removerAluno(indice) {
+  alunos = alunos.filter((_, i) => i !== indice);
+  renderLista();
+}
+
+// Função de cadastro
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const nome = document.getElementById("nome").value.trim();
+  const idade = document.getElementById("idade").value.trim();
+
+  if (!nome || !idade) return;
+
+  alunos.push({ nome, idade });
+
+  form.reset();
+  renderLista();
 });
+
